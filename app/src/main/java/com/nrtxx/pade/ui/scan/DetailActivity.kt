0f6009e6660ddee7405141f3c2638fdaa56782db
Rangga Nurta Kusumah <rangganurtakusumah@gmail.com>
2022-06-03 06:29:30 +0700

    connect to database

--- app/src/main/java/com/nrtxx/pade/ui/scan/DetailActivity.kt
+++ app/src/main/java/com/nrtxx/pade/ui/scan/DetailActivity.kt
@@ -1,13 +1,11 @@
 package com.nrtxx.pade.ui.scan
 
-import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContracts
-import com.nrtxx.pade.R
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.nrtxx.pade.databinding.ActivityDetailBinding
-import com.nrtxx.pade.helper.rotateBitmap
-import java.io.File
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
@@ -20,6 +18,27 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val db = Firebase.firestore
+
+        val docRef = db.collection("penyakit").document("BD")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
+                    binding.tvNameDetail.text = document.getString("Nama")
+                    binding.tvPenyebabDetail.text = document.getString("Penyebab")
+                    binding.tvGejalaDetail.text = document.getString("Gejala")
+                    binding.tvInfoDetail.text = document.getString("Info")
+                    binding.tvHADDetail.text = document.getString("HAD")
+                    binding.tvHAVDetail.text = document.getString("HAV")
+                } else {
+                    Log.d("notExist", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("errorDB", "get failed with ", exception)
+            }
     }
 
 }
